1afd0259997070a31a3eb562dc425a72d83b0ffb
midlynj <jmidlyn@gmail.com>
2022-06-13 15:26:27 -0500

    writing 1st test tdd

--- code.js
+++ code.js
@@ -2,3 +2,15 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function foo() {
+    return 2;
+}
+
+function sayHello() {
+
+}
+
+function isTwo() {
+    return 1 + 1;
+}
--- tests.js
+++ tests.js
@@ -13,3 +13,27 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe("foo", function() {
+    it("should return true if 2 === 2", function() {
+       let expectedResult = 2;
+       let actualResult = 2;
+        expect(actualResult).toBe(expectedResult);
+    });
+});
+
+describe("sayHello",function() {
+    it('should be a defined function', function() {
+    expect(typeof sayHello).toBe('function');
+
+    });
+});
+
+describe("isTwo", function() {
+    it("should return sum of 1 + 1", function () {
+        let expectedResult = 2;
+        let actualResult = 2;
+        expect(actualResult).toBe(expectedResult);
+
+    })
+})
